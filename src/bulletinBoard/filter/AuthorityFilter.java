a2dd341ee3c89af2bf8432101b274c33557184d4
shiho-fujiya <s_fujiya@ic-com.co.jp>
2016-11-22 15:26:56 +0900

    掲示板システム

--- src/bulletinBoard/filter/AuthorityFilter.java
+++ src/bulletinBoard/filter/AuthorityFilter.java
@@ -1,6 +1,8 @@
 package bulletinBoard.filter;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -11,6 +13,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import bulletinBoard.beans.User;
 
@@ -23,11 +26,16 @@ public class AuthorityFilter implements Filter{
 	  String target = ((HttpServletRequest)request).getRequestURI();
 	  //System.out.println(target);
 	  String[] urlBody = target.split("/");
+
 	  //要素数から数えるので-1しないと全ての要素が取れない
 	  if(urlBody[urlBody.length - 1].matches("management|settings|signup")) {
 		  User user = (User) ((HttpServletRequest)request).getSession().getAttribute("loginUser");
 		  if (user != null && user.getPositionId() != 1) {
-			  ((HttpServletResponse)response).sendRedirect("./home");
+			  List<String> messages = new ArrayList<String>();
+			  HttpSession session = ((HttpServletRequest)request).getSession();
+			  messages.add("権限がありません");
+			  session.setAttribute("errorMessages", messages);
+			  ((HttpServletResponse)response).sendRedirect("./");
 			  return;
 		  }
 	  }
